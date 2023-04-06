ddd945f882f3379399fdf00544a7163f81f42a06
Renatas Shidlovskas <Renat2766@users.noreply.github.com>
2023-04-06 16:57:11 +0600

    подключение Swagger и оформление API

--- src/plases/plases.controller.ts
+++ src/plases/plases.controller.ts
@@ -1,17 +1,35 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreatePlasesDto } from './dto/create-plases.dto';
 import { PlasesService } from './plases.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Plase } from './plases.model';
 
 @Controller('plases')
 export class PlasesController {
   
   constructor(private plaseService: PlasesService ) {}
 
+  @ApiOperation({
+    summary: 'Создание нового объекта',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Объект создан',
+    type: Plase
+  })
   @Post()
   create(@Body() plaseDto: CreatePlasesDto){
     return this.plaseService.createPlase(plaseDto);
   }
 
+  @ApiOperation({
+    summary: 'Получение всех объектов',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Объекты получены',
+    type: [Plase]
+  })
   @Get()
   getAll(){
     return this.plaseService.getAllPlases();
--- src/plases/plases.model.ts
+++ src/plases/plases.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 interface PlaseCreationAttrs {
@@ -9,12 +10,16 @@ interface PlaseCreationAttrs {
 
 @Table({tableName: 'plases'})
 export class Plase extends Model <Plase, PlaseCreationAttrs> {
+  @ApiProperty({example: 1, description: 'Уникальный идентификатор'})
   @Column({type: DataType.INTEGER, primaryKey: true, autoIncrement: true, unique: true})
   id: number;
+  @ApiProperty({example: 'Каток Медео', description: 'Заголовок объекта'})
   @Column({type: DataType.STRING, unique: true, allowNull: false})
   title: string;
+  @ApiProperty({example: 'Самый высокогорный каток', description: 'Описание объекта'})
   @Column({type: DataType.STRING,allowNull: false})
   description: string;
+  @ApiProperty({example: './dist/images/img1.jpg', description: 'Изображение объекта'})
   @Column({type: DataType.STRING,allowNull: false})
   image: string;
 }
